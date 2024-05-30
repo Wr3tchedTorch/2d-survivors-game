ff879a9d5c14f6c8e05240087c35c7d335bf6de2
Wr3tchedTorch <ericmoura511@gmail.com>
2024-05-30 15:36:12 -0300

    player movement and camera smoothing added to the game

---
+++ scenes/game-camera/GameCamera.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public partial class GameCamera : Camera2D
+{
+	private Vector2 _targetPosition = Vector2.Zero;
+
+	public override void _Ready() => MakeCurrent();
+
+	public override void _Process(double delta)
+	{
+		AcquireTarget();
+		GlobalPosition = GlobalPosition.Lerp(_targetPosition, 1f - (float) Math.Exp(-delta * 10));
+	}
+
+	private void AcquireTarget()
+	{
+		var playerNodes = GetTree().GetNodesInGroup("player");
+		if (playerNodes.Count <= 0) return;
+		
+		var player = (Player)playerNodes[0];
+
+		_targetPosition = player.GlobalPosition;		
+	}
+}
---
+++ scenes/player/Player.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody2D
+{
+	private const int _MAX_SPEED = 200;
+
+	public override void _Ready()
+	{
+		
+	}
+
+	public override void _Process(double delta)
+	{
+		Vector2 dir = GetMovementVector().Normalized();
+		Velocity = dir * _MAX_SPEED;
+		MoveAndSlide();
+	}
+
+	public Vector2 GetMovementVector() => Input.GetVector("move_left", "move_right", "move_up", "move_down");
+}
